ff87db63a7225bdb367240a91e516c053efbf5cb
comfuture <comfuture@iplateia.com>
2025-03-04 12:25:33 +0900

    fix: try fix token retrieval method in service worker

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 const fb = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 let token = null;
-firebase.getToken(messaging, { vapidKey: 'BAK5VZaNTmajMt_ob4UO1JKcVm6yhhNZvWDZ1OiNkfS8lE2yaomrhnOue9sQgbMKdGk3e_oVUM3cr_-8vfMZXSI' })
+messaging.getToken(messaging, { vapidKey: 'BAK5VZaNTmajMt_ob4UO1JKcVm6yhhNZvWDZ1OiNkfS8lE2yaomrhnOue9sQgbMKdGk3e_oVUM3cr_-8vfMZXSI' })
     .then((currentToken) => {
         if (currentToken) {
             console.log('Token:', currentToken);
